e864c92347348bccdcc1145b067d0f2e986758c0
Ben Dwyer <ben@scruffian.com>
2021-09-14 20:54:04 +0100

    rebuild

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1630795577';
+var cache = 'javascripture.22.0.1631649217';
 import xhr from 'xhr';
 import {
 	addColumnHelper,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'javascripture.22.0.1630795577';
+var cache = 'javascripture.22.0.1631649217';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
