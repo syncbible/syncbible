15f4a2a359423390383ec86b8e5952a31b04339c
Ben Dwyer <ben@scruffian.com>
2022-01-26 17:46:35 +0000

    update version number

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1639004985';
+var cache = 'syncbible.23.0.1643219174';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
