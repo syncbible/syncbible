2a9787c7e186262734cbe6a4b73b8cbfb8830a25
Ben Dwyer <ben@scruffian.com>
2022-03-16 23:13:22 +0000

    hide text from search

--- src/lib/copy-to-clipboard-helper.js
+++ src/lib/copy-to-clipboard-helper.js
@@ -1,11 +1,15 @@
 const copyToClipboardHelper = ( event, textToCopy ) => {
+	let finalText = textToCopy;
 	if ( textToCopy.current ) {
-		textToCopy = textToCopy.current.innerText.replace(/([0-9])(?:\r\n|\r|\n)/g, '$1. ' );
+		const parentElement = textToCopy.current.parentElement;
+		parentElement.style.display = 'block';
+		finalText = textToCopy.current.innerText.replace(/([0-9])(?:\r\n|\r|\n)/g, '$1. ' );
+		parentElement.style.display = 'none';
 	}
 
 	event.stopPropagation();
 	const textarea = document.createElement( 'textarea' )
-	textarea.value = textToCopy;
+	textarea.value = finalText;
 	document.body.appendChild( textarea );
 	textarea.select();
 	document.execCommand('copy');
