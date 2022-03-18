ab67790f6ebf021552413c0b02632248dfb597f5
Ben Dwyer <ben@scruffian.com>
2022-03-18 21:05:35 +0000

    fix verse copying

--- src/lib/copy-to-clipboard-helper.js
+++ src/lib/copy-to-clipboard-helper.js
@@ -2,9 +2,10 @@ const copyToClipboardHelper = ( event, textToCopy ) => {
 	let finalText = textToCopy;
 	if ( textToCopy.current ) {
 		const parentElement = textToCopy.current.parentElement;
+		const savedDisplay = parentElement.style.display;
 		parentElement.style.display = 'block';
 		finalText = textToCopy.current.innerText.replace(/([0-9])(?:\r\n|\r|\n)/g, '$1. ' );
-		parentElement.style.display = 'none';
+		parentElement.style.display = savedDisplay;
 	}
 
 	event.stopPropagation();
