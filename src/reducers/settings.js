0338b9caa95a48072d0986ac8bbbc331bed66209
Ben Dwyer <ben@scruffian.com>
2022-03-23 23:13:13 +0000

    fix interface language empty

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -7,7 +7,7 @@ const initialState = {
 	inSync: true,
 	expandedSearchResults: false,
 	highlightSearchResults: false,
-	interfaceLanguage: false,
+	interfaceLanguage: 'KJV',
 	type: "SETTINGS_CHANGE",
 	darkMode: null,
 	compareMode: false,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1647637541';
+var cache = 'syncbible.23.0.1648077158';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
