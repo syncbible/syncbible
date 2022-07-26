de0374469f922c3652a554f55e6efedd0738d287
Ben Dwyer <ben@scruffian.com>
2022-07-26 20:19:27 +0100

    rebuild

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1653428502';
+var cache = 'syncbible.23.0.1658863149';
 import xhr from 'xhr';
 import {
 	addColumnHelper,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1653428502';
+var cache = 'syncbible.23.0.1658863149';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
