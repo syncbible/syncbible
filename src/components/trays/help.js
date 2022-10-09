98ce51be9ef9346261653b3dbc2421241bd850de
Ben Dwyer <ben@scruffian.com>
2022-10-09 13:28:06 +0100

    fix the LC export

--- src/components/trays/help.js
+++ src/components/trays/help.js
@@ -13,7 +13,7 @@ const HelpTray = React.memo( () => {
 
 	const exportLC = () => {
 		const element = document.createElement("a");
-		const file = new Blob([ JSON.stringify( LC, null, "\t" ) ], {type: 'text/plain'});
+		const file = new Blob([ JSON.stringify( javascripture.data.LC, null, "\t" ) ], {type: 'text/plain'});
 		element.href = URL.createObjectURL(file);
 		element.download = "LC.json";
 		document.body.appendChild(element); // Required for this to work in FireFox
