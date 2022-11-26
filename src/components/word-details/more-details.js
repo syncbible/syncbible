e9e6177c2f9fdd1bae8b798c742abf224c204361
Ben Dwyer <ben@scruffian.com>
2022-11-26 22:34:28 +0000

    create a new word more component to avoid rerenders

---
+++ src/components/word-details/more-details.js
@@ -0,0 +1,127 @@
+// External dependencies
+import map from 'lodash/map';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { motion } from "framer-motion"
+
+// Internal dependencies
+import KJVDef from './kjv-def';
+import morphology from '../../lib/morphology';
+import stripPointing from '../../lib/strip-pointing.js';
+import styles from './styles.scss';
+import { getFamily } from '../../lib/word';
+import WordBlockLink from './word-block-link';
+
+const MoreDetails = ( { morphologyProp, strongsNumber, version } ) => {
+	const strongsDictionary = useSelector( state => state.data.strongsDictionary );
+	const strongsWithFamilies = useSelector( state => state.data.strongsObjectWithFamilies );
+	const [ showDetails, setShowDetails ] = useState( false );
+
+	const getBranchesData = () => {
+		return strongsWithFamilies && map( strongsWithFamilies, ( strongsObjectData, strongsObjectNumber ) => {
+			if ( strongsObjectData.roots && strongsObjectData.roots.indexOf( strongsNumber ) > -1 ) {
+				return (
+					<WordBlockLink key={ strongsObjectNumber } strongsNumber={ strongsObjectNumber } version={ version } />
+				);
+			}
+		} ).filter( branches => branches );
+	};
+
+	const getBranches = () => {
+		const branchesData = getBranchesData();
+		if ( branchesData && branchesData.length > 0 ) {
+			return branchesData;
+		}
+
+		return 'None';
+	}
+
+	const getRoots = () => {
+		if ( ! strongsWithFamilies || ! strongsWithFamilies[ strongsNumber ] ) {
+			return;
+		}
+
+		const rootsData = strongsWithFamilies[ strongsNumber ].roots;
+		if( rootsData ) {
+			return rootsData.map( ( rootNumber, index ) => {
+				return (
+					<WordBlockLink key={ index } strongsNumber={ rootNumber } version={ version } />
+				);
+			} );
+		}
+
+		return 'None';
+	};
+
+	const getMorphology = () => {
+		return morphologyProp && morphologyProp.split( ' ' ).map( ( morph, index ) => {
+			return ( index !== 0 ? ' - ' : '' ) + morphology( morph, 'noLinks', strongsNumber );
+		} );
+	};
+
+	const wordDetail = strongsDictionary && strongsDictionary[ strongsNumber ];
+	const getKJVDefinitions = () => {
+		return wordDetail && wordDetail.kjv_def && wordDetail.kjv_def.split( ',' ).map( ( word, index ) => {
+			const wordString = word.trim().replace( /\./g, '' );
+
+			return (
+				<span key={ index }>
+					{ index === 0 ? '' : ', ' }
+					<KJVDef word={ wordString } strongsNumber={ strongsNumber } />
+				</span>
+			);
+		} );
+	}
+
+	const getToggleDetails = () => (
+		<p><a className={ styles.moreDetails } href="#" onClick={ ( event ) => {
+			event.preventDefault();
+			setShowDetails( ! showDetails );
+		} }>
+			{ showDetails ? 'Hide' : 'View '} details
+		</a></p>
+	);
+
+	const variants = {
+		open: { height: 'auto' },
+		closed: { height: "0" },
+	}
+
+	return (
+		<>
+			<motion.div
+				initial="closed"
+				animate={ showDetails ? "open" : "closed" }
+				variants={ variants }
+				style={{ overflow: 'hidden' }}
+			>
+				{ strongsNumber } | { wordDetail && stripPointing( wordDetail.lemma ) }
+				{ wordDetail && wordDetail.xlit ? ' | ' + wordDetail.xlit : null }
+				{ wordDetail && wordDetail.translit ? ' | ' + wordDetail.translit : null }
+				{ wordDetail && wordDetail.pronounciation ? ' | ' + wordDetail.pronounciation : null }
+				<br />
+				<div>
+					<strong>Roots: </strong>{ getRoots() }
+				</div>
+				<div>
+					<strong>Branches: </strong>{ getBranches() }
+				</div>
+				<div>
+					<strong>Family: </strong>{ getFamily( strongsNumber, strongsWithFamilies ) }
+				</div>
+				<br />
+				<div>
+					<strong>Morphology</strong><br />{ morphologyProp } - { morphologyProp && getMorphology() }<br />
+					<br />
+					<strong>KJV translations</strong><br />{ getKJVDefinitions( strongsNumber ) }<br />
+					<br />
+					<strong>Strong's Derivation</strong><br />{ wordDetail && wordDetail.derivation }<br />
+				</div>
+				<br />
+			</motion.div>
+			{ getToggleDetails() }
+		</>
+	);
+};
+
+export default React.memo( MoreDetails );
--- src/components/word-details/word-block-details.js
+++ src/components/word-details/word-block-details.js
@@ -1,82 +1,18 @@
 // External dependencies
-import map from 'lodash/map';
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { motion } from "framer-motion"
 
 // Internal dependencies
-import KJVDef from './kjv-def';
-import morphology from '../../lib/morphology';
-import stripPointing from '../../lib/strip-pointing.js';
 import styles from './styles.scss';
-import { getFamily } from '../../lib/word';
-import WordBlockLink from './word-block-link';
 import SearchLink from '../search/search-link';
 import InlineResultsToggle from '../inline-results-toggle'
 import { searchForWord } from '../../actions'
+import MoreDetails from './more-details';
 
 const WordBlockDetails = ( { morphologyProp, strongsNumber, version, word } ) => {
 	const dispatch = useDispatch();
 	const interfaceLanguage = useSelector( state => state.settings.interfaceLanguage );
-	const strongsDictionary = useSelector( state => state.data.strongsDictionary );
 	const strongsWithFamilies = useSelector( state => state.data.strongsObjectWithFamilies );
-	const [ showDetails, setShowDetails ] = useState( false );
-
-	const getBranchesData = () => {
-		return strongsWithFamilies && map( strongsWithFamilies, ( strongsObjectData, strongsObjectNumber ) => {
-			if ( strongsObjectData.roots && strongsObjectData.roots.indexOf( strongsNumber ) > -1 ) {
-				return (
-					<WordBlockLink key={ strongsObjectNumber } strongsNumber={ strongsObjectNumber } version={ version } />
-				);
-			}
-		} ).filter( branches => branches );
-	};
-
-	const getBranches = () => {
-		const branchesData = getBranchesData();
-		if ( branchesData && branchesData.length > 0 ) {
-			return branchesData;
-		}
-
-		return 'None';
-	}
-
-	const getRoots = () => {
-		if ( ! strongsWithFamilies || ! strongsWithFamilies[ strongsNumber ] ) {
-			return;
-		}
-
-		const rootsData = strongsWithFamilies[ strongsNumber ].roots;
-		if( rootsData ) {
-			return rootsData.map( ( rootNumber, index ) => {
-				return (
-					<WordBlockLink key={ index } strongsNumber={ rootNumber } version={ version } />
-				);
-			} );
-		}
-
-		return 'None';
-	};
-
-	const getMorphology = () => {
-		return morphologyProp && morphologyProp.split( ' ' ).map( ( morph, index ) => {
-			return ( index !== 0 ? ' - ' : '' ) + morphology( morph, 'noLinks', strongsNumber );
-		} );
-	};
-
-	const wordDetail = strongsDictionary && strongsDictionary[ strongsNumber ];
-	const getKJVDefinitions = () => {
-		return wordDetail && wordDetail.kjv_def && wordDetail.kjv_def.split( ',' ).map( ( word, index ) => {
-			const wordString = word.trim().replace( /\./g, '' );
-
-			return (
-				<span key={ index }>
-					{ index === 0 ? '' : ', ' }
-					<KJVDef word={ wordString } strongsNumber={ strongsNumber } />
-				</span>
-			);
-		} );
-	}
 
 	const results = word.results && word.results.map( ( result, index ) => {
 		const isActive = word && typeof word.current !== 'undefined' && word.current === index;
@@ -100,7 +36,7 @@ const WordBlockDetails = ( { morphologyProp, strongsNumber, version, word } ) =>
 
 		if ( word.loading ) {
 			return (
-				<p>Loading { numberOfUses } search { resultString }.</p>
+				<p>Loading { numberOfUses } search { resultString }...</p>
 			);
 		}
 
@@ -112,59 +48,12 @@ const WordBlockDetails = ( { morphologyProp, strongsNumber, version, word } ) =>
 			<p><a href="#" className="word-block-details__find-all-uses" onClick={ () => dispatch( searchForWord( word.data ) ) }>Find { numberOfUses } { useString } { numberOfUses > 1000 && <span>(slow!)</span> }</a></p>
 		);
  	}
-	const toggleDetails = (
-		<p><a className={ styles.moreDetails } href="#" onClick={ ( event ) => {
-			event.preventDefault();
-			setShowDetails( ! showDetails );
-		} }>
-			{ showDetails ? 'Hide' : 'View '} details
-		</a></p>
-	);
-	const variants = {
-		open: { height: 'auto' },
-		closed: { height: "0" },
-	}
-	const moreDetails = (
-		<motion.div
-			initial="closed"
-			animate={ showDetails ? "open" : "closed" }
-			variants={ variants }
-			style={{ overflow: 'hidden' }}
-		>
-			{ strongsNumber } | { wordDetail && stripPointing( wordDetail.lemma ) }
-			{ wordDetail && wordDetail.xlit ? ' | ' + wordDetail.xlit : null }
-			{ wordDetail && wordDetail.translit ? ' | ' + wordDetail.translit : null }
-			{ wordDetail && wordDetail.pronounciation ? ' | ' + wordDetail.pronounciation : null }
-			<br />
-			<div>
-				<strong>Roots: </strong>{ getRoots() }
-			</div>
-			<div>
-				<strong>Branches: </strong>{ getBranches() }
-			</div>
-			<div>
-				<strong>Family: </strong>{ getFamily( strongsNumber, strongsWithFamilies ) }
-			</div>
-			<br />
-			<div>
-				<strong>Morphology</strong><br />{ morphologyProp } - { morphologyProp && getMorphology() }<br />
-				<br />
-				<strong>KJV translations</strong><br />{ getKJVDefinitions( strongsNumber ) }<br />
-				<br />
-				<strong>Strong's Derivation</strong><br />{ wordDetail && wordDetail.derivation }<br />
-			</div>
-			<br />
-		</motion.div>
-	);
-
-	let resultsDisplay = getResultsDisplay();
 
 	return (
 		<div className={ styles.wordBlocDetails }>
-			{ moreDetails }
-			{ toggleDetails }
+			<MoreDetails morphologyProp={ morphologyProp } strongsNumber={ strongsNumber } version={ version } />
 			<br />
-			{ resultsDisplay }
+			{ getResultsDisplay() }
 		</div>
 	);
 }
