c1baeac6def2f0d02cf6bf2969c640049f98e97f
Ben Dwyer <ben@escruffian.com>
2023-01-15 04:58:31 +0400

    add reverse cross refs

--- src/lib/cross-references.js
+++ src/lib/cross-references.js
@@ -13,10 +13,27 @@ export const getCrossReferencesArray = ( data, reference ) => {
 	return data.crossReferences[ referenceString ];
 };
 
+function getReverseCrossRefs( data, reference ) {
+	if ( ! data.crossReferences ) {
+		return [];
+	}
+
+	const bookId = bible.getBookId( reference.book );
+	const referenceString = bible.Data.books[ bookId - 1 ][ 1 ] + '.' + reference.chapter + '.' + reference.verse;
+	return Object.keys( data.crossReferences ).filter( crossReference => {
+		return data.crossReferences[ crossReference ].indexOf( referenceString ) > -1;
+	} );
+}
 
 export const getCrossReferences = ( data, reference ) => {
 	const crossReferenceArray = getCrossReferencesArray( data, reference );
-	return crossReferenceArray.map( ( referenceString ) => {
+	const reverseCrossRefs = getReverseCrossRefs( data, reference );
+
+	// Combine both sets of cross refs.
+	const combinedCrossRefs = crossReferenceArray.concat( reverseCrossRefs );
+	const uniqueCrossRefs = [ ...new Set( combinedCrossRefs ) ];
+
+	return uniqueCrossRefs.map( ( referenceString ) => {
 		const referenceSections = referenceString.split('-');
 		const referenceArray = referenceSections[0].split('.');
 		const bookId = bible.getBookId( referenceArray[0] );
