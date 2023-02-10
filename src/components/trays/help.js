fb67db56609c974dde0b9e9f88e96fafd7068ab3
scruffian <ben@scruffian.com>
2023-02-10 18:03:29 +0000

    Update details for Mark's Hebrew Literal

--- src/components/trays/help.js
+++ src/components/trays/help.js
@@ -50,6 +50,7 @@ const HelpTray = () => {
 				<p><a href="https://github.com/javascripture/javascripture/blob/gh-pages/data/literalConsistent.js">Literal consistent</a> | <a onClick={ () => exportFile( 'LC' ) }>export</a></p>
 				<p><a href="https://github.com/syncbible/syncbible/blob/trunk/bibles/NMV_strongs.js">NMV_strongs</a> | <a onClick={ () => exportFile( 'NMV_strongs') }>export</a></p>
 				<p>ESV: The Holy Bible, English Standard Version ©2011 Crossway Bibles, a division of Good News Publishers. All rights reserved.</p>
+				<p><a href="https://www.dropbox.com/s/7e05iklpkrtn46n/Genesis%20-%20Jeremiah.pdf?dl=0">Mark's Hebrew Literal</a>.<br/> <a href="https://www.dropbox.com/s/lrhzis4d9532gf4/Introduction.pdf?dl=0">Please read the introduction</a>.</p>
 				<p><br />Version: { typeof( javascripture.sw ) !== 'undefined' ? javascripture.sw : null }</p>
 				<p><a href="#" onClick={ clear }>Clear settings and start over</a></p>
 				<p>Built in Firefox.<br />Tested in Chrome.</p>
