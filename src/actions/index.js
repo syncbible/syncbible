9da2b38254a1d1e7cd10aab4d48e7eeebaee84d0
scruffian <ben@scruffian.com>
2023-02-13 23:43:36 +0000

    rebuild

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1676324340';
+var cache = 'syncbible.23.0.1676331788';
 
 // External dependencies.
 import xhr from 'xhr';
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1676324340';
+var cache = 'syncbible.23.0.1676331788';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
