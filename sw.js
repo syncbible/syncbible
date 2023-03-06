0500360e2f5ded3d2e111393bd3da0bc5cbe1f84
scruffian <ben@scruffian.com>
2023-03-06 14:17:06 +0000

    rebuild

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cache = 'syncbible.23.0.1677626654';
+var cache = 'syncbible.23.0.1678112215';
 
 self.addEventListener('install', function( e ) {
 	e.waitUntil( caches.open( cache ).then(function(cache) {
