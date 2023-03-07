70f1fc174e35dbc4e9e92617418a1b921b753622
scruffian <ben@scruffian.com>
2023-03-07 09:27:02 +0000

    add stats to search

---
+++ src/components/search/search-stats.js
@@ -0,0 +1,26 @@
+// External dependencies
+import React, { useState } from "react";
+import { groupBy, orderBy } from "lodash";
+import { useSelector, useDispatch } from "react-redux";
+import classnames from 'classnames';
+
+// Internal dependencies.
+import { goToReferenceAction } from '../../actions';
+import styles from "./styles.scss";
+import { getReferenceFromSearchResult, getGroupedResults } from '../../lib/reference';
+import SortGroupResults from "../sort-group-results";
+
+const SearchStats = ( { results, data } ) => {
+	if ( ! results ) {
+		return <p>Stats will appear when you have searched for the word.</p>;
+	}
+
+	return (
+		<div className={ styles.wordStats }>
+			<h2>Stats for { data.version }</h2>
+			<SortGroupResults results={ results } initialGroup="book" initialSort="reference" supportsWord={ false } />
+		</div>
+	);
+};
+
+export default React.memo( SearchStats );
