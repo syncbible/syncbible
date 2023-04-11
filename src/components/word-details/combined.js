646af086acfedd7a8093e122c1b56c1e98e3b3e8
scruffian <ben@scruffian.com>
2023-04-11 20:26:40 +0100

    prettier

--- src/components/word-details/combined.js
+++ src/components/word-details/combined.js
@@ -1,55 +1,58 @@
 // External dependencies
-import React, { useState, useMemo } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Internal dependencies
-import { setTrayVisibilityFilter } from "../../actions";
-import Collapsible from "../collapsible";
-import JoinFull from "../svg/join-full";
-import SortGroupResults from "../sort-group-results";
-import { getCombinedResults } from "../../lib/reference";
+import { setTrayVisibilityFilter } from '../../actions';
+import Collapsible from '../collapsible';
+import JoinFull from '../svg/join-full';
+import SortGroupResults from '../sort-group-results';
+import { getCombinedResults } from '../../lib/reference';
 
 const CombinedResults = ({ type }) => {
-  const dispatch = useDispatch();
-  const [open, setOpen] = useState(false);
-  const list = useSelector((state) => state.list);
-  const words = list.filter(({ listType }) => listType === type);
-  const results = words.map(({ results }) => {
-    return results;
-  });
-  const combinedResults = useMemo(() => getCombinedResults(results), [results]);
+	const dispatch = useDispatch();
+	const [open, setOpen] = useState(false);
+	const list = useSelector((state) => state.list);
+	const words = list.filter(({ listType }) => listType === type);
+	const results = words.map(({ results }) => {
+		return results;
+	});
+	const combinedResults = useMemo(
+		() => getCombinedResults(results),
+		[results]
+	);
 
-  if (words.length < 2) {
-    return null;
-  }
+	if (words.length < 2) {
+		return null;
+	}
 
-  return (
-    <Collapsible
-      open={open}
-      onToggle={() => setOpen(!open)}
-      className="collapse"
-      header={
-        <div>
-          Combined{" "}
-          <a
-            onClick={(event) => {
-              event.stopPropagation();
-              dispatch(setTrayVisibilityFilter("combinedall"));
-            }}
-          >
-            <JoinFull />
-          </a>
-        </div>
-      }
-    >
-      <SortGroupResults
-        results={combinedResults}
-        initialGroup="verse"
-        initialSort="desc"
-        allowPreview={true}
-      />
-    </Collapsible>
-  );
+	return (
+		<Collapsible
+			open={open}
+			onToggle={() => setOpen(!open)}
+			className="collapse"
+			header={
+				<div>
+					Combined{' '}
+					<a
+						onClick={(event) => {
+							event.stopPropagation();
+							dispatch(setTrayVisibilityFilter('combinedall'));
+						}}
+					>
+						<JoinFull />
+					</a>
+				</div>
+			}
+		>
+			<SortGroupResults
+				results={combinedResults}
+				initialGroup="verse"
+				initialSort="desc"
+				allowPreview={true}
+			/>
+		</Collapsible>
+	);
 };
 
 export default React.memo(CombinedResults);
