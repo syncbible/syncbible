ac44be3be8cb335c2a0891890cacf7c00f195c52
scruffian <ben@scruffian.com>
2023-04-11 21:39:14 +0100

    try to avoid rerendering search results

---
+++ src/reducers/user-interface.js
@@ -0,0 +1,20 @@
+const userInterface = (state = {}, action) => {
+	switch (action.type) {
+		case 'ADD_TO_LIST':
+			const dataForId = Object.assign({}, action.item.data);
+			delete dataForId.clickedWord;
+			delete dataForId.morphology;
+			const id = JSON.stringify(dataForId);
+			state[id] = true;
+			return { ...state };
+
+		case 'TOGGLE_LIST_ITEM_VISIBLE':
+			state[action.item.id] = !state[action.item.id];
+			return { ...state };
+
+		default:
+			return state;
+	}
+};
+
+export default userInterface;
