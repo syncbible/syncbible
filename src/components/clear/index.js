5c00a425e49779ab192d1c44a3bb4ccc9d8f5dd9
scruffian <ben@scruffian.com>
2023-04-16 11:42:08 +0100

    performance improvement

--- src/components/clear/index.js
+++ src/components/clear/index.js
@@ -3,56 +3,64 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 // Internal dependencies
-import { clearAll, removeTypeFromList, clearSearch } from '../../actions';
+import { removeTypeFromList } from '../../actions';
 
-import ClearSvg from '../svg/clear.js';
 import DeleteForever from '../svg/delete-forever.js';
 import styles from './style.scss';
 
-const Clear = ( { selectedTrayId } ) => {
+const Clear = ({ selectedTrayId }) => {
 	const dispatch = useDispatch();
-	const words = useSelector( state => state.list.filter( ( { listType } ) => listType === 'word' ) );
-	const bookmarks = useSelector( state => state.list.filter( ( { listType } ) => listType === 'bookmark' ) );
-	const searchTerms = useSelector( state => state.list.filter( ( { listType } ) => listType === 'search' ) );
+	const list = useSelector((state) => state.list);
+	const words = list.filter(({ listType }) => listType === 'word');
+	const bookmarks = list.filter(({ listType }) => listType === 'bookmark');
+	const searchTerms = list.filter(({ listType }) => listType === 'search');
 
-	if (  selectedTrayId !== 'bookmarks' && selectedTrayId !== 'word' && selectedTrayId !== 'search' ) {
-		return <span className={ styles.button }>&nbsp;</span>;
+	if (
+		selectedTrayId !== 'bookmarks' &&
+		selectedTrayId !== 'word' &&
+		selectedTrayId !== 'search'
+	) {
+		return <span className={styles.button}>&nbsp;</span>;
 	}
 
-	if ( selectedTrayId === 'bookmarks' && bookmarks.length === 0 ) {
-		return <span className={ styles.button }>&nbsp;</span>;
+	if (selectedTrayId === 'bookmarks' && bookmarks.length === 0) {
+		return <span className={styles.button}>&nbsp;</span>;
 	}
 
-	if ( selectedTrayId === 'word' && words.length === 0 ) {
-		return <span className={ styles.button }>&nbsp;</span>;
+	if (selectedTrayId === 'word' && words.length === 0) {
+		return <span className={styles.button}>&nbsp;</span>;
 	}
 
-	if ( selectedTrayId === 'search' && searchTerms.length === 0 ) {
-		return <span className={ styles.button }>&nbsp;</span>;
+	if (selectedTrayId === 'search' && searchTerms.length === 0) {
+		return <span className={styles.button}>&nbsp;</span>;
 	}
 
-	const clearTray = ( event ) => {
+	const clearTray = (event) => {
 		event.preventDefault();
-		if ( selectedTrayId === 'bookmarks' ) {
-			dispatch( removeTypeFromList( 'bookmark' ) );
+		if (selectedTrayId === 'bookmarks') {
+			dispatch(removeTypeFromList('bookmark'));
 		}
 
-		if ( selectedTrayId === 'word' ) {
-			dispatch( removeTypeFromList( 'word' ) );
+		if (selectedTrayId === 'word') {
+			dispatch(removeTypeFromList('word'));
 		}
 
-		if ( selectedTrayId === 'search' ) {
-			dispatch( removeTypeFromList( 'search' ) );
+		if (selectedTrayId === 'search') {
+			dispatch(removeTypeFromList('search'));
 		}
 	};
 
 	return (
-		<button onClick={ ( event ) => {
-			clearTray( event )
-		} } title="Clear" className={ styles.clear }>
+		<button
+			onClick={(event) => {
+				clearTray(event);
+			}}
+			title="Clear"
+			className={styles.clear}
+		>
 			<DeleteForever />
 		</button>
 	);
 };
 
-export default React.memo( Clear );
+export default React.memo(Clear);
